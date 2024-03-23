e2eaaaf178f7913c067ef8def297185aa4bf7149
Nazar <nazaruddinnurcharis28@gmail.com>
2024-03-23 11:10:29 +0700

    first commit

---
+++ app/Http/Controllers/CalculatorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Calculation;
+
+class CalculatorController extends Controller
+{
+    public function calculate(Request $request)
+    {
+        $operasi = $request->input('operasi');
+        $bil_pertama = (int) $request->input('bil_1');
+        $bil_kedua = (int) $request->input('bil_2');
+        $result = 0;
+
+        if ($operasi == "tambah") {
+            $result = $bil_pertama + $bil_kedua;
+        } else if ($operasi == "kurang") {
+            $result = $bil_pertama - $bil_kedua;
+        } else if ($operasi == "bagi") {
+            $result = $bil_pertama / $bil_kedua;
+        } else if ($operasi == "kali") {
+            $result = $bil_pertama * $bil_kedua;
+        } else if ($operasi == "mod") {
+            $result = $bil_pertama % $bil_kedua;
+        } else {
+            $result = 0;
+        }
+
+        // Buat instance baru dari model Calculation dan simpan data
+        $calculation = new Calculation;
+        $calculation->operation = $operasi;
+        $calculation->operand1 = $bil_pertama;
+        $calculation->operand2 = $bil_kedua;
+        $calculation->result = $result;
+        $calculation->save(); // Menyimpan data ke database
+
+
+        return redirect('/')->with('info', 'hasil nya adalah : ' . $result);
+    }
+
+    public function destroy($id)
+    {
+        // Cari kalkulasi berdasarkan ID dan hapus
+        $calculation = Calculation::findOrFail($id);
+        $calculation->delete();
+
+        // Kembali ke halaman utama dengan pesan
+        return back()->with('info', 'Kalkulasi berhasil dihapus');
+    }
+}
